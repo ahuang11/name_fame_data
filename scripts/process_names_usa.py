367dc98b7338bd13d8986dcea7ad53bc982fc221
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-01-17 13:46:00 -0700

    Update process_names_usa.py

--- scripts/process_names_usa.py
+++ scripts/process_names_usa.py
@@ -10,6 +10,11 @@ import pandas as pd
 import numpy as np
 
 
+SCRIPTS_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(SCRIPTS_DIR, "..", "data")
+os.makedirs(DATA_DIR, exist_ok=True)
+
+
 class BabyNames:
     def __init__(self):
         self._df = self._retrieve_df()
@@ -20,9 +25,10 @@ class BabyNames:
         self._recent_year = self._latest_year - 3  # 4 years ago
 
         self._name_groupby = self._df.groupby("name")
+        self._name_year_groupby = self._df.groupby(["name", "year"])
 
         self._yearly_df = (
-            self._get_count(self._df.groupby(["name", "year"]))
+            self._get_count(self._name_year_groupby)
             .pipe(lambda df: df.reindex(pd.MultiIndex.from_product(df.index.levels)))
             .reset_index()
         ).fillna(0)
@@ -37,7 +43,13 @@ class BabyNames:
         ]
 
     @staticmethod
-    def _retrieve_df():
+    def _retrieve_df(cache=True):
+        cache_path = os.path.join(DATA_DIR, "names.pkl")
+        if cache and os.path.exists(cache_path):
+            print(f"using {cache_path}")
+            df = pd.read_pickle(cache_path)
+            return df
+
         url = "https://www.ssa.gov/oact/babynames/state/namesbystate.zip"
         columns = ["state", "gender", "year", "name", "count"]
         print(f"opening {url}")
@@ -52,9 +64,11 @@ class BabyNames:
                             print(f"reading {txt_file}")
                             df = pd.read_csv(txt_data, header=None, names=columns)
                             df_list.append(df)  # append to list
-        df = pd.concat(df_list)  # combine
+        df = pd.concat(df_list).sort_values("year")  # combine
 
-        return df.sort_values("year")
+        if cache:
+            df.to_pickle(cache_path)
+        return df
 
     @staticmethod
     def _get_count(name_groupby):
@@ -89,7 +103,7 @@ class BabyNames:
     @property
     def overall_female_rank(self):
         return self._get_rank(
-            self._get_count(self._get_gender(self._df, "M").groupby("name"))
+            self._get_count(self._get_gender(self._df, "F").groupby("name"))
         )
 
     @property
@@ -154,7 +168,8 @@ class BabyNames:
     @property
     def percent_male(self):
         return (
-            self._get_count(self._get_gender(self._df, "M")) / self.overall_count
+            self._get_count(self._get_gender(self._df, "M").groupby("name"))
+            / self.overall_count
         ) * 100
 
     @property
@@ -170,24 +185,36 @@ class BabyNames:
         return self._flatten_list(self._get_mode(self._name_groupby, "state"))
 
     @property
-    def median_age(self):
-        return self._this_year - self._name_groupby["year"].median()
+    def first_year(self):
+        return self._name_groupby["year"].min()
+
+    @property
+    def final_year(self):
+        return self._name_groupby["year"].max()
 
     @property
     def average_year(self):
         return self._name_groupby["year"].mean()
 
     @property
+    def peak_year(self):
+        return (
+            self._get_count(self._name_year_groupby)
+            .reset_index()
+            .pipe(
+                lambda df: df.loc[df.groupby("name")["count"].idxmax()].set_index(
+                    "name"
+                )["year"]
+            )
+        )
+
+    @property
+    def median_age(self):
+        return self._this_year - self._name_groupby["year"].median()
+
+    @property
     def average_age(self):
         return self._this_year - self.average_year
-
-    @property
-    def first_appearance(self):
-        return self._name_groupby["year"].min()
-
-    @property
-    def final_appearance(self):
-        return self._name_groupby["year"].max()
 
     @property
     def dataframe(self):
@@ -200,35 +227,33 @@ class BabyNames:
             ],
             axis=1,
         )
+        for col in df.columns:
+            if "rank" in col:
+                df[col] = df[col].fillna(df[col].max() + 1)
         df = df.fillna(0)
         number_cols = list(df.select_dtypes(np.number).columns)
-        number_cols.remove("overall_count")  # too big of a number
-        number_cols.remove("latest_count")  # too big of a number
-        df[number_cols] = df[number_cols].astype(np.int16)
-        df[["overall_count", "latest_count"]] = df[
-            ["latest_count", "overall_count"]
-        ].astype(int)
+        df[number_cols] = df[number_cols].astype(int)
         return df
 
-    def export(self):
-        # create data dir if not exists
-        print("creating data dir")
-        scripts_dir = os.path.dirname(os.path.realpath(__file__))
-        data_dir = os.path.join(scripts_dir, "..", "data")
-        os.makedirs(data_dir, exist_ok=True)
-
-        stats_db = os.path.join(data_dir, "names_stats.db")
+    def export(self, cache=True):
+        stats_db = os.path.join(DATA_DIR, "names_stats.db")
         if os.path.exists(stats_db):
             print(f"removing {stats_db}")
             os.remove(stats_db)
 
+        # export stats
+        stats_df = self.dataframe
+        if cache:
+            cache_path = os.path.join(DATA_DIR, "names_stats.pkl")
+            print(f"exporting {cache_path}")
+            stats_df.to_pickle(cache_path)
         with sqlite3.connect(stats_db) as con:
             print(f"exporting {stats_db}")
-            self.dataframe.to_sql("names", con)
+            stats_df.to_sql("names", con)
 
         # shard by starting letter
         for char in self._df["name"].str[0].unique():
-            char_db = os.path.join(data_dir, f"{char}_names_recs.db".lower())
+            char_db = os.path.join(DATA_DIR, f"{char}_names_recs.db".lower())
             if os.path.exists(char_db):
                 print(f"removing {char_db}")
                 os.remove(char_db)
